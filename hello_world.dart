b272d9adeecef4559084d1c6e3f38260d60cf1dc
Mohamed Elfalaky <106435178+MohamedElfalaky@users.noreply.github.com>
2024-07-03 18:55:41 +0300

    first commit

---
+++ hello_world.dart
@@ -0,0 +1,32 @@
+/* 
+
+1- what is version control?
+* Version control is a system that tracks changes to files over time.
+
+2- what is git.
+Git is a distributed version control system
+
+3-what is github, gitlab, azure?
+GitHub is a web-based platform that uses Git for version control. It provides a user-friendly interface to manage Git repositories, collaborate with others, and host projects
+
+
+
+////////////////////////////
+* Terms:
+1- Repository (repo) : مشروع مرفوع على جيت هاب
+
+
+
+
+
+////////////////////////////
+
+* How to Upload a project to github for the first time?
+
+
+ */
+
+
+
+
+

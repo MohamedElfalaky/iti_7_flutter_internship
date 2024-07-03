4ad7623e0b9b81fa7ff52962d61c59a9a4e461d3
Mohamed Elfalaky <106435178+MohamedElfalaky@users.noreply.github.com>
2024-07-03 21:01:21 +0300

    launch version two

--- hello_world.dart
+++ hello_world.dart
@@ -46,7 +46,9 @@ How to Upload a project to github (after first time)?
 2- git commit -m "رسالة من اختيارك"
 3- git push 
 4- git pull (from github)
-5- git branch
+5- git branch branck name (create new branch)
+6- git branch (show current branches)
+7- git checkout branch name (navigate to a branch)
 
  */
 

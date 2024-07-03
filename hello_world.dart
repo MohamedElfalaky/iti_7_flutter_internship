3e33ae95f18ea5a597f987c1e3cc69cf22c51181
Mohamed Elfalaky <106435178+MohamedElfalaky@users.noreply.github.com>
2024-07-03 19:02:43 +0300

    update code

--- hello_world.dart
+++ hello_world.dart
@@ -29,7 +29,11 @@ send files from local repository(folder) to working area (to be tracked).
 3- git commit -m "رسالة مختصرة عن سبب الرفع"
 send files from working area to staging area.
 
+4- git branch -M main (one time only for each project).
 
+5- git remote add origin https://github.com/MohamedElfalaky/iti_7_flutter_internship.git (first time only)
+
+6- git push -u origin main (first time only.. it will be git push)
  */
 
 
